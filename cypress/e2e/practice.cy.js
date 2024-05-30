c956eb4048a24ec9d9c1e36c3c5ae56def66967a
Akshayk9230 <akshay00923@gmail.com>
2024-05-30 19:52:25 +0530

    first commit

---
+++ cypress/e2e/practice.cy.js
@@ -0,0 +1,10 @@
+describe('This is a Practice Test Suite', () => {
+  it('Validate Title of the webPage', () => {
+    cy.visit('https://rahulshettyacademy.com/AutomationPractice/')
+    cy.title().should('eq','Practice Page')
+  })
+  it('To Click on radio buttons', () => {
+    cy.visit('https://rahulshettyacademy.com/AutomationPractice/')
+    cy.xpath('//*[@value="radio1"]').click()
+  })
+})
